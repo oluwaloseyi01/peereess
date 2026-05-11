6799576dfefb726c86a588c939a44d8ef2f9e3f7
oluwaseyi omotunde <oluwaloseyifunmi001@gmail.com>
2026-05-11 23:13:21 +0100

    add flutter project

--- android/app/src/main/kotlin/com/example/peereess/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/peereess/MainActivity.kt
@@ -1,13 +1,5 @@
-import io.flutter.embedding.android.FlutterActivity
-import android.os.Bundle
+package com.peereess.app
 
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-    
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.clear() // prevent TransactionTooLargeException
-    }
-}
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
